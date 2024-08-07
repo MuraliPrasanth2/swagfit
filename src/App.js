f40bf9ed91cacb7e630419ade24824b5f5496201
Murali Prasanth <mprasanth18@gmail.com>
2024-08-07 22:23:24 +0530

    removed the protection for the book page for testing

--- src/App.js
+++ src/App.js
@@ -14,17 +14,18 @@ function App() {
 		return <PrivateRoute redirect={redirect}>{element}</PrivateRoute>;
 	};
 
+	// <Route
+	// 	path="/book"
+	// 	exact
+	// 	element={wrapPrivateRoute(<Book />, "/book")}
+	// />
 	return (
 		authReady && (
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login/*" element={<OtpLogin />} />
-					<Route
-						path="/book"
-						exact
-						element={wrapPrivateRoute(<Book />, "/book")}
-					/>
+					<Route path="/book" exact element={<Book />} />
 					<Route
 						path="/groupfitness"
 						exact

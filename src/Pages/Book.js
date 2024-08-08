8f8c9d8b4d8e9e48e377f2a284455e8bde54a2b8
Murali Prasanth <mprasanth18@gmail.com>
2024-08-08 13:59:31 +0530

    modified the button styles in Book.js

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -2,6 +2,15 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const OnlineGroupFitnessProgramPricingSection = () => {
+    // <button
+    //     className="bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-semibold py-2 px-4 rounded"
+    //     onClick={() => {
+    //         navigate("/groupfitness");
+    //     }}
+    // >
+    //     Get Started
+    // </button>
+
     const navigate = useNavigate();
 
     return (
@@ -19,14 +28,14 @@ const OnlineGroupFitnessProgramPricingSection = () => {
                 <li className="mb-2 ml-3">🕺 Pilates and dance fitness</li>
                 <li className="mb-2 italic">⏰ Flexible schedule</li>
             </ul>
-            <button
-                className="bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-semibold py-2 px-4 rounded"
+            <div
+                className="button-rainbow-container mx-auto mt-9 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:block hover:before:blur-lg active:before:blur-lg z-[1] before:-z-10"
                 onClick={() => {
                     navigate("/groupfitness");
                 }}
             >
-                Get Started
-            </button>
+                <button className="button-rainbow z-[3] w-60">Get Started</button>
+            </div>
         </div>
     );
 };
@@ -36,23 +45,25 @@ const PersonalFitnessTrainingPricingSection = () => {
         <div className="bg-neutral-900 p-8 rounded-lg shadow-lg w-full max-w-96 self-stretch flex flex-col justify-evenly">
             <h3 className="text-xl font-semibold mb-4">Personal fitness program</h3>
             <p className="text-gray-400 mb-4">
-                Achieve your fitness goals with customized diet and workout plans and
-                personal guidance
+                Transform Your Fitness Journey with Personalized Online Training!
             </p>
             <p className="text-4xl font-bold mb-4">
-                9000 ₹<span className="text-base">/mo</span>
+                ₹ 7000 <span className="text-base">/mo</span>
             </p>
             <ul className="mb-8">
-                <li className="mb-2">Personalized workout plans.</li>
-                <li className="mb-2">Personalized diet plan</li>
-                <li className="mb-2">
-                    Personalized guidance as per your fitness goals
+                <li className="mb-2 font-bold">Benefits of tailored workouts</li>
+                <li className="mb-2 ml-3">
+                    💪 Expert guidance, and Flexible scheduling
                 </li>
-                <li className="mb-2">One-on-one fitness sessions</li>
+                <li className="mb-2 ml-3">🏠 All from the comfort of your home</li>
+                <li className="mb-2 italic text-sm mt-3">
+                    Our 26-session package is designed to help you achieve your fitness
+                    goals efficiently and effectively.
+                </li>
             </ul>
-            <button className="bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-semibold py-2 px-4 rounded">
-                Get Started
-            </button>
+            <div className="button-rainbow-container mx-auto mt-9 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:block hover:before:blur-lg active:before:blur-lg z-[1] before:-z-10">
+                <button className="button-rainbow z-[3] w-60">Get Started</button>
+            </div>
         </div>
     );
 };
@@ -62,20 +73,21 @@ const PersonalDanceProgram = () => {
         <div className="bg-neutral-900 bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-96 self-stretch flex flex-col justify-evenly">
             <h3 className="text-xl font-semibold mb-4">Dance Fitness Program</h3>
             <p className="text-gray-400 mb-4">
-                Stay fit and have fun with energetic dance workouts
+                Elevate your fitness journey with high-energy online dance sessions.
             </p>
             <p className="text-4xl font-bold mb-4">
-                4000 ₹<span className="text-base">/mo</span>
+                ₹ 7000 <span className="text-base">/mo</span>
             </p>
             <ul className="mb-8">
-                <li className="mb-2">5 Dance fitness sessions / week</li>
-                <li className="mb-2">Includes Zumba, Hip Hop, and Salsa</li>
-                <li className="mb-2">Boosts cardiovascular health and endurance</li>
-                <li className="mb-2">Enhances coordination and rhythm</li>
+                <li className="mb-2">🕺 Stay fit with freestyle and hip hop dance</li>
+                <li className="mb-2">
+                    🫶 Tailored workouts that enhance flexibility Coordination, and
+                    Confidence.
+                </li>
             </ul>
-            <button className="bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-semibold py-2 px-4 rounded mt-auto">
-                Get Started
-            </button>
+            <div className="button-rainbow-container mx-auto mt-9 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:block hover:before:blur-lg active:before:blur-lg z-[1] before:-z-10">
+                <button className="button-rainbow z-[3] w-60">Get Started</button>
+            </div>
         </div>
     );
 };
@@ -85,20 +97,24 @@ const PersonalPhysioProgram = () => {
         <div className="bg-neutral-900 p-8 rounded-lg shadow-lg w-full max-w-96 self-stretch flex flex-col justify-evenly">
             <h3 className="text-xl font-semibold mb-4">Personal Physio Program</h3>
             <p className="text-gray-400 mb-4">
-                Recover and strengthen with tailored physiotherapy sessions
+                Reclaim Your Strength with Online Physio Rehab Sessions!
             </p>
             <p className="text-4xl font-bold mb-4">
-                5000 ₹<span className="text-base">/mo</span>
+                ₹ 7000 <span className="text-base">/mo</span>
             </p>
             <ul className="mb-8">
-                <li className="mb-2">Personalized rehabilitation exercises</li>
-                <li className="mb-2">Injury prevention and recovery plans</li>
-                <li className="mb-2">Expert guidance for musculoskeletal health</li>
-                <li className="mb-2">One-on-one physiotherapy sessions</li>
+                <li className="mb-2">
+                    🏠 Personalized rehabilitation from the comfort of your home
+                </li>
+                <li className="mb-2">
+                    🦾 Expert physiotherapist with tailored treatment plans
+                </li>
+                <li className="mb-2">🤝 Dedicated support for faster recovery</li>
+                <li className="mb-2">⏰ Flexible scheduling to fit your needs</li>
             </ul>
-            <button className="bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-semibold py-2 px-4 rounded mt-auto">
-                Get Started
-            </button>
+            <div className="button-rainbow-container mx-auto mt-9 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:block hover:before:blur-lg active:before:blur-lg z-[1] before:-z-10">
+                <button className="button-rainbow z-[3] w-60">Get Started</button>
+            </div>
         </div>
     );
 };
